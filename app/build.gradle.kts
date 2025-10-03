68fa534ddc296cde1e4c8a01fb352019f3a7028f
theopakieser <20101918@mail.wit.ie>
2025-10-03 14:50:52 +0100

    refactor finished, lab placemark00

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
 }
 
 android {
-    namespace = "org.wit.bookapp"
-    compileSdk = 36
+    namespace = "org.wit.bookapp"   // <-- change to your new app namespace
+    compileSdk = 34                 // 36 is preview, stick to latest stable (34)
 
     defaultConfig {
-        applicationId = "org.wit.bookapp"
-        minSdk = 30
-        targetSdk = 36
+        applicationId = "org.wit.bookapp"  // <-- update to match namespace
+        minSdk = 24                        // 30 is too restrictive unless required
+        targetSdk = 34                     // match compileSdk
         versionCode = 1
         versionName = "1.0"
 
@@ -26,22 +26,28 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
+    }
+
+    buildFeatures {
+        viewBinding = true  // <-- enable this for your btnAdd / EditText binding
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/org/wit/bookapp/BookAppActivity.kt
+++ app/src/main/java/org/wit/bookapp/BookAppActivity.kt
@@ -1,6 +1,5 @@
 package org.wit.bookapp
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -12,10 +11,10 @@ class BookAppActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_bookapp)
-       // ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-       //     val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-         //   v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-           // insets
-       // }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
