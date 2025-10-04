430dcfd093c3a369d6fff396811dc7cd2da06da8
theopakieser <20101918@mail.wit.ie>
2025-10-04 22:18:04 +0100

    Can now add and display number of pages in logcat

--- app/src/main/java/org/wit/bookapp/activities/BookAppActivity.kt
+++ app/src/main/java/org/wit/bookapp/activities/BookAppActivity.kt
@@ -27,12 +27,13 @@ class BookAppActivity : AppCompatActivity() {
             book.title = binding.bookTitle.text.toString()
             book.author = binding.bookAuthor.text.toString()
             book.genre = binding.bookGenre.text.toString()
-            if (book.title.isNotEmpty() || book.author.isNotEmpty() || book.genre.isNotEmpty() ) {
-                i("add Button Pressed: ${book.title} by ${book.author}. \n This is a ${book.genre} book")
+            book.pages = binding.bookPages.text.toString().toIntOrNull() ?: 0
+            if (book.title.isNotEmpty() && book.author.isNotEmpty() && book.genre.isNotEmpty() && book.pages > 0) {
+                i("add Button Pressed: ${book.title} by ${book.author}. \n This is a ${book.genre} book, it is ${book.pages} pages long")
             }
             else {
                 Snackbar
-                    .make(it,"Please Enter a title, author and genre", Snackbar.LENGTH_LONG)
+                    .make(it,"Please fill in all required fields", Snackbar.LENGTH_LONG)
                     .show()
             }
         }
